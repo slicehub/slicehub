69098ef45f58c845eb46c6d7d6014aef11675b28
Steven Molina <stevenmolina2205@gmail.com>
2025-12-11 13:16:34 -0300

    fix(): hardcoded sepolia usdc address

--- src/config/index.ts
+++ src/config/index.ts
@@ -22,4 +22,13 @@ export const wagmiAdapter = new WagmiAdapter({
 
 export const config = wagmiAdapter.wagmiConfig;
 
-export const USDC_ADDRESS = "0x036CbD53842c5426634e7929541eC2318f3dCF7e"; // Base Sepolia USDC
+type AppEnv = "development" | "staging" | "production";
+const env = (process.env.NEXT_PUBLIC_APP_ENV || "development") as AppEnv;
+
+const USDC_ADDRESSES = {
+  development: "0x036CbD53842c5426634e7929541eC2318f3dCF7e", // Base Sepolia
+  staging: "0x036CbD53842c5426634e7929541eC2318f3dCF7e", // Base Sepolia
+  production: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // Base Mainnet
+};
+
+export const USDC_ADDRESS = USDC_ADDRESSES[env] || USDC_ADDRESSES.development;
